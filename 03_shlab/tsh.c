b37b41f7188ed4158d5f6bf13ec303a54f6402b6
w3schools-test <test@w3schools.com>
2023-11-29 19:09:02 +0900

    test02_1

--- 03_shlab/tsh.c
+++ 03_shlab/tsh.c
@@ -174,28 +174,28 @@ void eval(char *cmdline)
   char *argv[MAXARGS];
   char buf[MAXLINE];
   int bg;
-  pdi_t pid;
+  pid_t pid;
 
   strcpy(buf, cmdline);
   bg = parseline(buf, argv);
 
   if (argv[0] == NULL)
     return;
-  if(strcmp(argv[0],"quit") && !strcmp(argv[0],"fg") && !strcmp(argv[0],"bg") && strcmp(argv[0],jobs)){
-    if((pid=Fork())==0){
+  if(strcmp(argv[0],"quit") && !strcmp(argv[0],"fg") && !strcmp(argv[0],"bg") && strcmp(argv[0],"jobs")){
+    if((pid=fork())==0){
       if(execve(argv[0],argv,environ)<0){
         printf("%s: Command not found.\n", argv[0]);
         exit(0);
       }
     }
 
-    if(!bd){
+    if(!bg){
       int status;
       if(waitpid(pid, &status, 0)<0)
         unix_error("waitfg: waitpid error");
     }
     else 
-      printf("%d %d",pid,cmdline);
+      printf("%d %s",pid,cmdline);
   }
   else
     builtin_cmd(argv);
