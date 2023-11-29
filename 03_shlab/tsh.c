ccb3308e26b63ad4e0c466fbc6c7967c3d2853fd
w3schools-test <test@w3schools.com>
2023-11-29 23:37:27 +0900

    testing fork function

--- 03_shlab/tsh.c
+++ 03_shlab/tsh.c
@@ -186,8 +186,8 @@ void eval(char *cmdline)
     pid = fork();
     printf("  ##forked! child: %d  current: %d  bg: %d \n",pid,getpid(),bg);//######################3
     if(pid==0){
-      exit(0);
-      printf("  ##child\n"); //###################
+      // exit(0);
+      // printf("  ##child\n"); //###################
       if(execve(argv[0],argv,environ)<0){
         printf("%s: Command not found.\n", argv[0]);
         deletejob(jobs, getpid());
